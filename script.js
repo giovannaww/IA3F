2952d741d259eed32b7430d888b3af6f4f355cd0
giovannaww <163446075+giovannaww@users.noreply.github.com>
2024-08-01 16:49:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-reultado');
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativa:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+]
