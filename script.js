618f5225a4bcf514021eed44f224a121c3129f82
giovannaww <163446075+giovannaww@users.noreply.github.com>
2024-09-19 16:48:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
-const caixaAlternativas = document.querySelector('.caixa-alternativa');
-const caixaResultado = document.querySelector('.caixa-reultado');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
 
-const perguntas = [
+const perguntas = [ //serve para abrir lista de perguntas
+    {//abre o objeto das perguntas
+        enunciado: "Você acredita na Inteligência Artificial?",
+        alternativas: [" SIM ","NÃO "]
+    },
     {
-        enunciado: "pergunta 1",
-        alternativa:[ "alternativa 1", "alternativa 2"],
+        enunciado: "A inteligência artificial pode ser considerada confiavel no meio da Medicina?",
+        alternativas: [" SIM "," NÃO "]
+    },
+    {
+        enunciado: "Você acredita que pode utilizar a inteligencia artificial no dia a dia?",
+        alternativas: [" SIM "," NÃO "]
+    },
+    {
+        enunciado: " Você pode usar a inteligencia artificial para abrir seu proprio negocio?",
+        alternativas: [" SIM "," NÃO "]
+    },
+    {
+        enunciado: "A Inteligencia Artificial é útil na sua rotina? ",
+        alternativas: [" SIM "," NÃO "]
+    },
+    {
+        enunciado: "Daqui 10 anos você acredita que a inteligencia artificial pode estar dominando o mundo?",
+        alternativas: [" SIM "," NÃO "]
+    },
+]
 
-         enunciado: "pergunta 2",
-         alternativa:[ "alternativa 1", "alternativa" ]
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
-] 
+}
+mostraPergunta();
